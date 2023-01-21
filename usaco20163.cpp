116c6893aca0319932b7a392be7a5dfa999cccf9
spol-29 <polineni.sriram9@gmail.com>
2023-01-20 22:14:38 -0600

    cow signal

---
+++ usaco20163.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	ofstream cout ("cowsignal.out");
+	ifstream cin ("cowsignal.in");
+	int n,m,k;
+	cin>>m>>n>>k;
+    for(int i=0; i<m; i++){
+		string s;
+		cin>>s;
+		for(int t=0; t<k; t++){
+	        for(int j=0; j<n; j++){
+			    for(int x=0; x<k; x++){
+			    	cout<<s[j];
+			    }
+		    }
+		    cout<<endl;	
+        }
+	}
+}
