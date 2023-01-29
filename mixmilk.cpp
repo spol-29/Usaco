ed5a0788d59a634ca13be88599a2069fa733c43e
spol-29 <polineni.sriram9@gmail.com>
2023-01-29 10:31:07 -0600

    Usaco Problems(Basic)

---
+++ mixmilk.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+#define ll long long;
+using namespace std;
+int main(){
+    ll c1,m1,c2,m2,c3,m3;
+    cin>>c1>>m1>>c2>>m2>>c3>>m3;
+    
+}
--- shell.cpp
+++ shell.cpp
@@ -1,23 +1,36 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <cstdio>
+#include <vector>
+#include <algorithm>
 
-int main(){
-	
-	int n,a,b,g,hold = 0;
-	cin >> n;
-	vector<int> shell(3);
-	for(int i = 0; i < 3; i++){
-		shell[i] = i;
-	}
-	vector<int> c(3);
-	for(int i = 0; i < 3; i++){
-		cin >> a >> b >> g;
-		a--;
-		b--;
-		g--;
-		swap(shell[a],shell[b]);
-		hold = shell[g];
-		c[hold]++;
+using std::vector;
+
+int main() {
+	freopen("shell.in", "r", stdin);
+
+	int n;
+	scanf("%d", &n);
+
+	// shell_at_pos[i] stores the label of the shell located at position i
+	vector<int> shell_at_pos(3);
+	// Place the shells down arbitrarily
+	for (int i = 0; i < 3;i++) {
+		shell_at_pos[i] = i;
 	}
 	
+	// counter[i] stores the number of times the shell with label i was picked
+	vector<int> counter(3);
+	for (int i = 0; i < n; i++) {
+		int a, b, g;
+		scanf("%d %d %d", &a, &b, &g);
+		// Zero indexing: offset all positions by 1
+		a--, b--, g--;
+
+		// Perform Bessie's swapping operation
+		std::swap(shell_at_pos[a], shell_at_pos[b]);
+		// Count the number of times Elsie guesses each particular shell
+		counter[shell_at_pos[g]]++;
+	}
+	
+	freopen("shell.out", "w", stdout);
+	printf("%d\n", std::max({counter[0], counter[1], counter[2]}));
 }
