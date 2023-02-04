9e0264c5011123ca5b6d692376f2a607751e4665
spol-29 <polineni.sriram9@gmail.com>
2023-02-04 12:10:59 -0600

    badmilk - not solved

--- badmilk/badmilk.cpp
+++ badmilk/badmilk.cpp
@@ -1,11 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-	int n,m,d,s;
-    cin >> n >> m >> d >> s;
+int main(){ 
+	int n,m,d,s; //n = number of people, m = number of milk, d = number of drinks, s = number of sick people
+    cin >> n >> m >> d >> s; //input
+    for(int i = 0; i < d; i++){ //loop through all drinks
+        int p,m,t; //person, milk, time
+        cin >> p >> m >> t;
+        vector<int> people(d); //people who drank milk
+        vector<int> milk(d); //milk drank
+        vector<int> time(d); //time drank
+        people.push_back(p); //push back person
+        milk.push_back(m); //push back milk
+        time.push_back(t); //push back time
+    }
+    vector<int> sick(s); //people who got sick
+    vector<int> sicktime(s); //time they got sick
+    for(int i = 0; i < s; i++){
+        int p,t; //person and time
+        cin >> p >> t; //input
+        sick.push_back(p); //push back person 
+        sicktime.push_back(t); //push back time
+    }
+    //find the milk that was drank by the sick people
+    vector<int> sickmilk; //milk drank by sick people
     for(int i = 0; i < d; i++){
-        int p,m,t;
-        
-
+        if(people[i] == sick[i] && time < sicktime[i]){
+            sickmilk.push_back(milk[i]);
+        }
     }
 }
