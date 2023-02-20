473cd09559e1c055332e2384471eb82f3d1eb0d5
spol-29 <polineni.sriram9@gmail.com>
2023-02-20 14:56:41 -0600

    collge sleeping in class

--- college/college.cpp
+++ college/college.cpp
@@ -1,46 +1,29 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int n,MAX = 0;
-//     cin >> n;
-//     vector<int> cowTuitions;
-//     vector<int> maxTuitions;
-//     for(int i = 0; i < n; i++){
-//         int x;
-//         cin >> x;
-//         cowTuitions.push_back(x);
-//     }
-//     for(int i = 0; i < n; i++){
-//         if(cowTuitions[i] > MAX){
-//             MAX = cowTuitions[i];
-//         }
-//     }
-//     for(int i = 1; i <= MAX; i++){
-
-//     }
-// }
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    int c[n];
-    for (int i = 0; i < n; i++) {
-        cin >> c[i];
-    }
-    sort(c, c + n);
-    long long max_revenue = 0;
-    int optimal_tuition = 0;
-    for (int tuition = 1; tuition <= 1000000; tuition++) {
-        int num_attendees = upper_bound(c, c + n, tuition) - c;
-        long long revenue = tuition * num_attendees;
-        if (revenue > max_revenue) {
-            max_revenue = revenue;
-            optimal_tuition = tuition;
+int main(){
+   int a;
+   cin >> a;
+   vector<int> tuitions;
+   for(int i = 0; i < a; i++){
+        int b;
+        cin >> b;
+        tuitions.push_back(b);
+   }
+   int c = 0;
+   for(int i = 0; i < a; i++){
+        if(tuitions[i] > c){
+            c = tuitions[i];
         }
-    }
-    cout << max_revenue << " " << optimal_tuition << endl;
-    return 0;
+   }
+   for(int i = 1; i <= c; i++){
+        int d = 0;
+        for(int j = 0; j < a; j++){
+            if(tuitions[j] >= i){
+                d++;
+            }
+        }
+        if(d >= i){
+            cout << i << endl;
+        }
+   }
 }
---
+++ sleepingclass/sleepinginclass.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    vector<int> numtimes;
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+        int n;
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            int a;
+            cin >> a;
+            numtimes.push_back(a);
+        }
+        
+    }
+}
