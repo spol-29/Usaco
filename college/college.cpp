b2597019df377d2a52a23c2e6f76672d8c6306fa
spol-29 <polineni.sriram9@gmail.com>
2023-02-23 18:09:46 -0600

    college

--- college/college.cpp
+++ college/college.cpp
@@ -1,29 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   int a;
-   cin >> a;
-   vector<int> tuitions;
-   for(int i = 0; i < a; i++){
-        int b;
-        cin >> b;
-        tuitions.push_back(b);
-   }
-   int c = 0;
-   for(int i = 0; i < a; i++){
-        if(tuitions[i] > c){
-            c = tuitions[i];
+    int n,MAX = 0, cnt_tuition = 0,max_tuition = 0,tuition_cost;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> max_profits(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; i++){
+        MAX = max(MAX, a[i]);
+    }
+    for(int i = 1; i <= MAX; i++){
+        if(a[i] <= i){
+            cnt_tuition++;
         }
-   }
-   for(int i = 1; i <= c; i++){
-        int d = 0;
-        for(int j = 0; j < a; j++){
-            if(tuitions[j] >= i){
-                d++;
-            }
-        }
-        if(d >= i){
-            cout << i << endl;
-        }
-   }
+        max_profits[i-1] = cnt_tuition * i;
+        cnt_tuition = 0;
+    }
+    for(int i = 0; i < n; i++){
+        max_tuition = max(max_tuition, max_profits[i]);
+        tuition_cost = i+1;
+    }
+    cout << max_tuition << " " << tuition_cost;
 }
